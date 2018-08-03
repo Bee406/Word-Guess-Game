a23df68629e02e1a638ff0035bd0610113fd0d48
Bee <bee406@users.noreply.github.com>
2018-08-02 19:49:49 -0600

    edits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 // Create array for computer to choose from
-var wordChoices = ["panda", "zebra", "monkey", "dolphin", "penguin"];
+var wordChoices = ["panda", "zebra", "monkey", "dolphin", "penguin", "lion", "tiger", "elephant", "seal", "otter",];
 
 // Define click to start text
 var clickText = document.getElementById("start-dialog");
@@ -18,16 +18,12 @@ loseText.style.display = "none";
 var totalWins = [];
 var reducer = (accumulator, currentValue) => accumulator + currentValue;
 
-//Define guessed letters and list 
-var guessedLetters = [];
-var guessList = [];
-
 //Watch document for click to start game function
 document.addEventListener("click", gameStartFunction);
 
 function gameStartFunction() {
 
-    //Make start, win, and lose text dissapear
+    //Make start, win, and lose text dissapear. Could potentially use newdiv here??
     winText.style.display = "none";
     loseText.style.display = "none";
     clickText.style.display = "none";
@@ -38,15 +34,17 @@ function gameStartFunction() {
     //Check words are being randomly selected
     console.log(wordSelection);
 
+    //Define guessed letters
+    var guessedLetters = [];
+
     //Create underscores for number of letters in the selected word
     var spaces = [];
     for (var i = 0; i < wordSelection.length; i++) {
         spaces[i] = "_ ";
     };
 
-    //Get rid of commas, make string out of array?
-    var wordAnswer = document.getElementById("spaces");
-    wordAnswer.innerHTML = spaces.join(" ");
+    //Display spaced on screen
+    document.getElementById("word-spaces").innerHTML = spaces.join(" ");
 
 
     //Determine number of guesses the user will get
@@ -60,22 +58,37 @@ function gameStartFunction() {
     var win = 0;
 
     //Display guesses
-    var guessList = document.getElementById("guessed");
+    document.getElementById("letters-guessed").innerHTML = "Letters guessed: " + guessedLetters.join(" ");
 
 
     //Function for user to start guessing
     document.onkeyup = function gamePlayFunction() {
         if (guessesLeft > 0 && lettersLeft > 0) {
-            var userGuess = event.key;
+            var userGuess = event.key.toLowerCase(;
 
             //check if letter has been guessed 
             if (guessedLetters.indexOf(userGuess) === -1) {
+                //check each letter in the selected word to determine if guess is correct
+                for (var j = 0; j < wordSelection.length; j++) {
+                    if (userGuess === wordSelection[j]) {
+                        //index in selected word of correct guess
+                        console.log("Index in selected word: " + wordSelection.indexOf(userGuess));
+                        spaces[j] = userGuess;
+                        document.getElementById("word-spaces").innerHTML = spaces.join(" ");
+
+                        console.log("Letter guessed is in selected word");
+
+                        //subtract 1 from letters left to guess
+                        lettersLeft--
+                        console.log("Letters left to guess: " + lettersLeft);
+                    }
+                }
 
                 //add guess to array of guessed letters
                 guessedLetters.push(userGuess);
 
-                //put spaces between letters and disply on document
-                guessList.innerHTML = "Letters guessed: " + guessedLetters.join(" ");
+                //display guesses on document
+                document.getElementById("letters-guessed").innerHTML = "Letters guessed: " + guessedLetters.join(" ");
 
                 //subtract 1 from guesses left
                 guessesLeft--;
@@ -83,20 +96,6 @@ function gameStartFunction() {
                 //display updated guesses left
                 document.getElementById("guesses").innerHTML = "Number of guesses remaining: " + guessesLeft;
 
-
-                //check each letter in the selected word to determine if guess is correct
-                for (var j = 0; j < wordSelection.length; j++) {
-                    if (userGuess === wordSelection[j]) {
-                        console.log("Letter guessed is in selected word");
-
-                        //index in selected word of correct guess
-                        console.log(wordSelection.indexOf(userGuess));
-
-                        //subtract 1 from letters left to guess
-                        lettersLeft--
-                        console.log(lettersLeft);
-                    }
-                }
                 //game over if letters left = 0 (user guessed all letters in the word)
                 if (lettersLeft === 0) {
 
